9beede2775303fc4d8aef01d39cdf4a307d27bfb
AlexeyLeuhin <forface@tut.by>
2021-02-09 14:03:00 +0300

    task3 done

---
+++ task3/task3/GUI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class GUI
+    {
+
+        public static void ShowMenu(string[] possibleSteps)
+        {
+            for(int i = 0; i < possibleSteps.Length; i++)
+            {
+                Console.WriteLine((i+1) + " - " + possibleSteps[i]);
+            }
+            Console.WriteLine(0 + " - exit");
+        }
+
+        public static void RequireUsersTurn()
+        {
+            Console.WriteLine("Enter your move: ");
+        }
+
+        public static void ShowUserMove(string move)
+        {
+            Console.WriteLine("Your move: " + move);
+        }
+
+        public static void ShowComputerMove(string move)
+        {
+            Console.WriteLine("Computer move: " + move);
+        }
+
+        public static void ShowComputerMoveHmac(byte[] hmac)
+        {
+            Console.WriteLine("HMAC: " + Convert.ToBase64String(hmac));
+            
+        }
+
+        public static void ShowComputerMoveHMACKey(byte[] key)
+        {
+            Console.WriteLine("HMAC key: " + Convert.ToBase64String(key) + '\n');
+        }
+
+        public static void ShowUserWon()
+        {
+            Console.WriteLine("You win!!!");
+        }
+        public static void ShowComputerWon()
+        {
+            Console.WriteLine("Computer win(");
+        }
+
+
+        public static void ShowDraw()
+        {
+            Console.WriteLine("Draw, you have chosen the same move as computer!");
+        }
+
+        public static void Exit()
+        {
+            Console.WriteLine("GoodBye!");
+        }
+    }
+}
---
+++ task3/task3/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace task3
+{
+    class User
+    {
+        public int step { get; private set; }
+
+        public void MakeTurn(string[] possibleSteps)
+        {
+            try 
+            {
+                step = int.Parse(Console.ReadLine());
+                if(step < 0 || step > possibleSteps.Length)
+                {
+                    throw new Exception("Incorrect users input");
+                }
+            }
+            catch
+            {
+                throw new Exception("Incorrect users input");
+            }
+           
+        }
+
+    }
+}
