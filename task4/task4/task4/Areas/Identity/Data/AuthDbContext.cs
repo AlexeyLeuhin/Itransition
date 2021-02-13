7bd380f8a3e3ff6ce17794ae532d4291d22650e1
AlexeyLeuhin <forface@tut.by>
2021-02-13 13:38:05 +0300

    project added

---
+++ task4/task4/task4/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using task4.Areas.Identity.Data;
+
+namespace task4.Data
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);           
+        }
+    }
+}
